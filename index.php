66e59aa53e5fd19079fedbd200b7545ca5452e45
Your Name <you@example.com>
2020-03-09 14:13:50 +0100

    modif1

--- index.php
+++ index.php
@@ -7,7 +7,33 @@ $donnee = "Le PHP, c'est facile";
 
 $image ="php-leader1-880x450.png";
 
+$bonjour ="Bonjour";
+
+$formulaire = [
+    ['nom' => 'Dupont', 'prenom' => 'Jean', 'email'=> 'jdupont@gmail.com'],
+    ['nom' => 'Dupuis', 'prenom' => 'Loïc', 'email'=> 'loic45000@gmail.com'],
+    ['nom' => 'Armand', 'prenom' => 'Edgard', 'email'=> 'armand-e@gmail.com'],
+    ['nom' => 'Durand', 'prenom' => 'Emilie', 'email'=> 'emiliedurand@gmail.com'],
+    ['nom' => 'Martin', 'prenom' => 'Justine', 'email'=> 'juju289@gmail.com']
+];
+
+$new =['nom'=> $_GET['nom'], 'prenom' => $_GET['prenom'], 'email'=> $_GET['email']];
+var_dump($new);
+
+array_push($formulaire, $new);
+
+
+$nbelem = sizeof($formulaire);
+$liste = "";
+
+for ($i=0; $i<$nbelem; $i++){
+   $liste .= "<li>".$formulaire[$i]["nom"]. "</li>";
+}
+
+var_dump($_GET);
+
 ?>
+
 
 <DOCTYPE html>
 <html>
@@ -16,9 +42,9 @@ $image ="php-leader1-880x450.png";
 <title> Premiers pas en PHP</title>
 </head>
 <body>
-<h1><?php echo $titre?></h1>
-<p><?php echo $donnee?></p>
-<img src="<?php echo $image?>" alt="">
+
+<h1>Voici la liste des utilisateurs</h1>
+<ul><?= $liste?></ul>
 
 </body>
 </html>
